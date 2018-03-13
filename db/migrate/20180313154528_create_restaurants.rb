d703d6dc6ce2707b24d543c38510629c4faae1d2
Josué Estévez Fernández <jestfer6@gmail.com>
2018-03-13 15:50:18 +0000

    added restaurants migration. Next: seed it

---
+++ db/migrate/20180313154528_create_restaurants.rb
@@ -0,0 +1,11 @@
+class CreateRestaurants < ActiveRecord::Migration[5.1]
+  def change
+    create_table :restaurants do |t|
+      t.string :name
+      t.string :description
+      t.string :price
+      t.string :rating
+      t.string :price
+    end
+  end
+end
